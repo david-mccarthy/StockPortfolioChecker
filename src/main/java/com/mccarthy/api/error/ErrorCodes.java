16d913e9bd424859fb8260c1cdc23cd1512464d1
David McCarthy <davemac4821@gmail.com>
2021-10-23 10:51:07 +0100

    Move errors to MessageSource. (#3)

--- src/main/java/com/mccarthy/api/error/ErrorCodes.java
+++ src/main/java/com/mccarthy/api/error/ErrorCodes.java
@@ -9,6 +9,7 @@ public final class ErrorCodes {
     public static final String EXTERNAL_SERVICE_ERROR = "002";
     public static final String DUPLICATE_SYMBOLS_IN_REQUEST = "003";
     public static final String SYMBOL_VALIDATION_ERROR = "004";
+    public static final String INVALID_SYMBOL_ERROR = "004";
 
     public static final String UNKNOWN_ERROR_OCCURED = "999";
 }
--- src/test/java/com/mccarthy/api/unit/error/ErrorServiceTest.java
+++ src/test/java/com/mccarthy/api/unit/error/ErrorServiceTest.java
@@ -4,25 +4,39 @@ import com.mccarthy.api.error.ErrorService;
 import com.mccarthy.api.model.ApiError;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ErrorServiceTest {
 
     protected ErrorService errorService;
 
+    @Mock
+    private MessageSource messageSource;
+
     @Before
     public void setup() {
-        errorService = new ErrorService();
+        errorService = new ErrorService(messageSource);
     }
 
     @Test
     public void testCreateError() {
         String exceptionMessage = "Exception to test";
-        String errorCode = "Error Code";
+        String errorCode = "001";
         String errorMessage = "Message";
+        when(messageSource.getMessage(eq(errorCode), any(), any(Locale.class))).thenReturn(errorMessage);
         Exception e = new Exception(exceptionMessage);
-        ApiError apiError = errorService.createApiError(e, errorCode, errorMessage);
+        ApiError apiError = errorService.createApiError(e, errorCode);
 
         assertEquals("Error code incorrect.", errorCode, apiError.getErrorCode());
         assertEquals("Error message incorrect.", errorMessage, apiError.getErrorMessage());
